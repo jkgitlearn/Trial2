6b02c1945fb976dffbfcc1603113c2b1a8c8bdc7
jkgitlearn <shilpi_rams@yahoo.com>
2022-08-21 19:09:10 -0400

    added addinganothertestcase and modified demogitlearning2

---
+++ src/demoGitLearning/AddingAnotherTestCase.java
@@ -0,0 +1,11 @@
+package demoGitLearning;
+
+public class AddingAnotherTestCase {
+
+	public static void main(String[] args) {
+		
+		//this is my adding new test case/class from eclipse
+
+	}
+
+}
--- src/demoGitLearning/DemoGitLearning2.java
+++ src/demoGitLearning/DemoGitLearning2.java
@@ -1,2 +1,7 @@
 //this is a new test cass
 //this test will check editing 
+
+
+// this is another line added and modified from existing test case from eclipse 
+// this is another line added and modified from existing test case from eclipse 
+// this is another line added and modified from existing test case from eclipse 
